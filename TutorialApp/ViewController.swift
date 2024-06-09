0aa062a0a78be74ef6b40ce40b71847b2d1da8f0
Bruno Mazzocchi <brunomazzocchidev@gmail.com>
2024-06-09 15:21:06 -0600

    Basic IOS form

--- TutorialApp/ViewController.swift
+++ TutorialApp/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var fullNameLabel: UILabel!
+    @IBOutlet weak var generateFullNameButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func generateNameButton(_ sender: Any) {
+    guard
+        let firstName = firstNameTextField.text,
+        let lastName = lastNameTextField.text
+    else {
+        return
+    }
+    fullNameLabel.text = "\(firstName) \(lastName)"
+    }
 }
 
